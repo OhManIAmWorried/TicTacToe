9ebde3de09c8f9101d72bba4f0d08fdc04d4abcb
OhManIAmWorried <wladimirr04@gmail.com>
2016-11-05 12:59:21 +0200

    [0] Array changed to matrix for further operations with coordinates, comments added

--- Graphics/GameField.java
+++ Graphics/GameField.java
@@ -5,27 +5,28 @@ import java.awt.*;
 
 public class GameField extends JFrame{
 
-    private JPanel mainpanel = new JPanel();
-    private XOButton button[] = new XOButton[9];
+    private JPanel mainpanel = new JPanel();                         //Игровая панель на которую установленны кнопки
+    private XOButton button[][] = new XOButton[3][3];                //матрица кнопок
 
     public static void main(String[] args){
-        new GameField();
+        new GameField();                                             //создаём окно
     }
 
     public GameField(){
-        super("TicTacToe v 0.0");
-        setSize(400,400);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        mainpanel.setLayout(new GridLayout(3,3));
+        super("TicTacToe v 0.0");                                    //называем окно
+        setSize(400,400);                                            //устанавливаем размер окна (ширина x высота)
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);     //устанавливаем операции по выходу //TODO: Всплывающее сообщение "Вы уверенны?"
+        mainpanel.setLayout(new GridLayout(3,3));                    //устанавливаем разметку сеткой на рабочую поверхность
 
-        for (int i = 0; i < 9; i++){
-            button[i] = new XOButton();
-            mainpanel.add(button[i]);
-        }
+        for (int i = 0; i < 3; i++)                                  //добавляем кнопки на рабочую поверхность
+            for (int j = 0; j < 3; j++) {
+                button[i][j] = new XOButton();                       //TODO: Выбор своих и чужих символов для отображения
+                mainpanel.add(button[i][j]);
+            }
 
-        setAlwaysOnTop(true);
-        setResizable(false);
-        add(mainpanel);
-        setVisible(true);
+        setAlwaysOnTop(true);                                        //режим поверх всех окон
+        setResizable(false);                                         //режим статического размера
+        add(mainpanel);                                              //добавляем рабочую поверхность на окно
+        setVisible(true);                                            //режим видимости окна
     }
 }
