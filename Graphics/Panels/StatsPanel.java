96b0b4e916f94f25f6c65454d0eecc78c12375d0
OhManIAmWorried <wladimirr04@gmail.com>
2016-12-05 21:41:32 +0200

    Stats fixed

--- Graphics/Panels/StatsPanel.java
+++ Graphics/Panels/StatsPanel.java
@@ -14,7 +14,7 @@ public class StatsPanel extends JPanel {
     JLabel[][] label;
 
     public StatsPanel(){
-        setLayout(new GridLayout(4, 10));
+        setLayout(new GridLayout(10, 4));
         stat = new Statistic();
         Upgrade();
     }
@@ -23,7 +23,7 @@ public class StatsPanel extends JPanel {
         try {
             stat.readStats();
         } catch (Exception e){
-
+            e.printStackTrace();
         }
         label = new JLabel[4][stat.getSize()];
         Result[] res = stat.getStatsArr();
@@ -31,7 +31,7 @@ public class StatsPanel extends JPanel {
         for (int i = 0; i < stat.getSize(); i++) {
             System.out.println(i);
             System.out.println(res[i].Name1+"____"+res[i].Res+ "____"+res[i].Name2+ "____"+res[i].Time.getTime());
-          /*  for(int j=0; j<5; j++)
+            for(int j=0; j<4; j++)
                 label[j][i] = new JLabel();
             label[0][i].setText(res[i].Name1);
             label[1][i].setText(res[i].Res);
@@ -40,7 +40,7 @@ public class StatsPanel extends JPanel {
             this.add(label[0][i]);
             this.add(label[1][i]);
             this.add(label[2][i]);
-            this.add(label[3][i]);*/
+            this.add(label[3][i]);
         }
     }
 }
